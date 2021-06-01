7fa7b953371638200b401f5dbedf4c392787b0d0
Taylor Hogan <taylorb.hogan@gmail.com>
2021-06-01 14:31:20 -0700

    Create user validators

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,8 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db/models');
 const { csrfProtection, asyncHandler } = require('./utils');
-// const csurf = require('csurf');
+const {check, validationResult} = require('express-validator')
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
   const users = Users.FindAll()
   res.render('respond with a resource', {users});
@@ -18,7 +17,73 @@ router.get('/signup', csrfProtection, (req, res, next) =>{
   });
 });
 
-router.post('/signup', asyncHandler(async(req, res, next) =>{
+const signupValidators = [
+  check('firstName')
+    .exists({checkFalsy: true})
+    .withMessage('First name cannot be empty.')
+    .isLength({max: 50})
+    .withMessage('First name must be 50 characters or fewer.'),
+  check('lastName')
+    .exists({checkFalsy: true})
+    .withMessage('Last name cannot be empty.')
+    .isLength({max: 50})
+    .withMessage('Last name must be 50 characters or fewer.'),
+  check('username')
+    .exists({checkFalsy: true})
+    .withMessage('Username cannot be empty')
+    .isLength({max: 50})
+    .withMessage('Username must be 50 characters or fewer.')
+    .custom((username) => {
+      return db.User.findOne({where: {username}})
+        .then((user) => {
+          if (user) {
+            return Promise.reject('The provided username is already in use; please pick another.')
+          }
+        })
+      }),
+  check('email')
+    .exists({checkFalsy: true})
+    .withMessage('Email cannot be empty.')
+    .isLength({max: 255})
+    .withMessage('Email must be 255 characters or fewer.')
+    .isEmail()
+    .withMessage('Please enter a valid email address.')
+    .custom((email) => {
+      return db.User.findOne({where: {email}})
+        .then((user) => {
+          if (user) {
+            return Promise.reject('The provided email address is already in use; please pick another.')
+          }
+        })
+      }),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Password cannot be empty.')
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])/, 'g')
+    .withMessage('Password must contain at least 1 lowercase letter, uppercase letter, number, and special character (i.e. "!@#$%^&*")'),
+  
+]
+
+router.post('/signup', csrfProtection, asyncHandler(async(req, res, next) =>{
+  const {
+    firstName,
+    lastName,
+    username,
+    email,
+    password
+  } = req.body;
+
+  const user = await db.User.build({
+    firstName,
+    lastName,
+    username,
+    email
+  })
+
+  const signupErrors = validationResult(req)
+
+
+
   res.send('respond with a resource');
 }));
 
