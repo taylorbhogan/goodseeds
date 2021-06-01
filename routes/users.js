7be766d788c7b8d8b4011bcdd70524df120f394d
Justin <justinycho.business@gmail.com>
2021-06-01 13:39:37 -0700

    users pug created

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,27 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const db = require('../db/models');
+const { csrfProtection, asyncHandler } = require('./utils');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  const users = Users.FindAll()
+  res.render('respond with a resource', {users});
+});
+
+router.get('/signup', (req, res, next) =>{
+  const user = db.User.build();
+  res.render('users-signup');
+});
+
+router.post('/signup', asyncHandler(async(req, res, next) =>{
+  res.send('respond with a resource');
+}));
+
+router.get('/login', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+
 
 module.exports = router;
