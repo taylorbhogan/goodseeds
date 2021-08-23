9cf8f6526032b89dc9d0afc4582bc37d02480d89
Taylor Hogan <taylorb.hogan@gmail.com>
2021-08-23 13:51:46 -0400

    Set up backend for AWS S3. Add file input field in User Signup template

--- app.js
+++ app.js
@@ -20,8 +20,8 @@ const app = express();
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
-app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
