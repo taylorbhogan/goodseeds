8018d160161c9cef0da36276c90ce0e938e7d2aa
Taylor Hogan <taylorb.hogan@gmail.com>
2021-08-30 16:50:09 -0700

    Connect plant image upload

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var router = express.Router();
+const { asyncHandler } = require('./utils');
+const {
+  singleMulterUpload,
+  singlePublicFileUpload,
+} = require('../awsS3');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -14,6 +19,19 @@ router.get('/:id', function(req, res, next) {
   res.render('404');
 });
 
+router.post(
+  '/image',
+  singleMulterUpload('image'),
+  asyncHandler(async (req, res) => {
+    console.log('inside /image route ______-------------_______');
+    const imgUrl = await singlePublicFileUpload(req.file)
+    console.log('inside /image route ______-------------_______',imgUrl);
+    
+    return res.json({
+      imgUrl,
+    });
+  })
+);
 
 
 module.exports = router;
